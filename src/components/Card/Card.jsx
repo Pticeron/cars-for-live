0bb68e6cc295d8312ed1d69c32aeefff2293121f
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-09-18 21:39:57 +0300

    add card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+const Card = ({model,
+    make,
+    id,
+    img,
+    year,
+    address,
+    rentalPrice,
+    rentalCompany,
+    type,
+    functionalities,
+    fuelConsumption,
+    engineSize,
+    description,
+    accessories,
+    rentalConditions,
+    mileage,}) => {
+
+}
+
+export default Card;
