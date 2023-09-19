df5ef268c4158d97dbb0385475265979c8de59d7
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-09-19 19:58:35 +0300

    fix button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,33 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
+import Loader from './Loader/Loader';
+import { useGetCarsQuery } from 'redux/usersSlice';
 
-const About = lazy(() => import("../pages/About/About"));
+// const About = lazy(() => import("../pages/About/About"));
 const Home = lazy(() => import('../pages/Home/Home'));
 const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
-// const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
-// const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
-const Limitation = lazy(() => import('./services/Limitation'));
-const Payment = lazy(() => import('./services/Payment'));
-const Documents = lazy(() => import('./services/Documents'));
+const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+// const Limitation = lazy(() => import('./services/Limitation'));
+// const Payment = lazy(() => import('./services/Payment'));
+// const Documents = lazy(() => import('./services/Documents'));
 
 export const App = () => {
+  const { data } = useGetCarsQuery();
+  if (!data) return;
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="limitation" element={<Limitation />} />
-          <Route path="payment" element={<Payment />} />
-          <Route path="documents" element={<Documents />} />
-        </Route>
-        <Route path="catalog" element={<Catalog />} />
-        </Route>
-    </Routes>
+    <>
+      <Routes>
+
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home data={data}  />} />
+            <Route path="/catalog" element={<Catalog data={data} />} />
+            <Route path="/favorites" element={<Favorites data={data}  />} />
+          </Route>
+          <Route path="*" element={<Suspense fallback={<Loader />}><NotFoundPage /></Suspense>} />
+
+      </Routes>
+        </>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,62 @@
-import HeroSection from "components/Hero/Hero";
-import { ContentWrapper, HomeSection, HomeTextMain, HomeTextSecondary, HomeWrapper } from "./Home.styled";
+import HeroSection from 'components/Hero/Hero';
+import {
+  ContentWrapper,
+  HomeSection,
+  HomeTextMain,
+  HomeTextSecondary,
+  HomeWrapper,
+} from './Home.styled';
 
 const Home = () => {
- return (
-  <>
-    <HeroSection>
-      <HomeSection>
-        <HomeWrapper>
-          <ContentWrapper>
-            <HomeTextMain>
-            У сучасному суспільстві такі цінності як час і комфорт все більше виходять на передній план, тому така послуга як оренда автомобіля – вже перестала бути розкішшю, а стала щоденною необхідністю.
-
-Залежно від ситуації покупка машини не завжди є доцільним вкладенням коштів. Наприклад, для гостей Києва в туристичних, або бізнес цілях найкращим рішенням буде подобовий прокат авто – це недорого для вирішення тимчасових питань. Для резидентів столиці дана послуга також буде корисна під здійснення різного роду поїздок по Україні: оренда авто на весілля, відпочинок, фотосесію або просто в розважальних цілях.
-
-Життя непередбачуване і ніколи не знаєш, в яких ситуаціях може терміново знадобиться прокат автомобіля. Для вирішення даної проблеми існує сервіс компанії CarForLive – послуги автопрокату. Ми пропонуємо взяти машину в оренду на найвигідніших умовах – від 18 $ на добу.
-            </HomeTextMain>
-            <HomeTextSecondary>
-            In modern society, such values as time and comfort increasingly come to the fore, so a service such as car rental has ceased to be a luxury, but has become a daily necessity.
-
-Depending on the situation, buying a car is not always an appropriate investment. For example, for guests of Kyiv for tourist or business purposes, the best solution would be daily car rental - it is inexpensive to solve temporary issues. For residents of the capital, this service will also be useful for making various types of trips around Ukraine: renting a car for a wedding, vacation, photo shoot or just for entertainment purposes.
-
-Life is unpredictable and you never know in which situations you may urgently need a car rental. To solve this problem, there is a service of the CarForLive company - car rental services. We offer to rent a car on the most favorable terms - from $18 per day.
-            </HomeTextSecondary>
-          </ContentWrapper>
-        </HomeWrapper>
-      </HomeSection>
-    </HeroSection>
-  </>
- )
+  return (
+    <>
+      <HeroSection>
+        <HomeSection>
+          <HomeWrapper>
+            <ContentWrapper>
+              <HomeTextMain>
+                У сучасному суспільстві такі цінності як час і комфорт все
+                більше виходять на передній план, тому така послуга як оренда
+                автомобіля – вже перестала бути розкішшю, а стала щоденною
+                необхідністю. Залежно від ситуації покупка машини не завжди є
+                доцільним вкладенням коштів. Наприклад, для гостей Києва в
+                туристичних, або бізнес цілях найкращим рішенням буде подобовий
+                прокат авто – це недорого для вирішення тимчасових питань. Для
+                резидентів столиці дана послуга також буде корисна під
+                здійснення різного роду поїздок по Україні: оренда авто на
+                весілля, відпочинок, фотосесію або просто в розважальних цілях.
+                Життя непередбачуване і ніколи не знаєш, в яких ситуаціях може
+                терміново знадобиться прокат автомобіля. Для вирішення даної
+                проблеми існує сервіс компанії CarForLive – послуги автопрокату.
+                Ми пропонуємо взяти машину в оренду на найвигідніших умовах –
+                від 18 $ на добу.
+              </HomeTextMain>
+              <HomeTextSecondary>
+                In modern society, such values as time and comfort increasingly
+                come to the fore, so a service such as car rental has ceased to
+                be a luxury, but has become a daily necessity. Depending on the
+                situation, buying a car is not always an appropriate investment.
+                For example, for guests of Kyiv for tourist or business
+                purposes, the best solution would be daily car rental - it is
+                inexpensive to solve temporary issues. For residents of the
+                capital, this service will also be useful for making various
+                types of trips around Ukraine: renting a car for a wedding,
+                vacation, photo shoot or just for entertainment purposes. Life
+                is unpredictable and you never know in which situations you may
+                urgently need a car rental. To solve this problem, there is a
+                service of the CarForLive company - car rental services. We
+                offer to rent a car on the most favorable terms - from $18 per
+                day.
+              </HomeTextSecondary>
+            </ContentWrapper>
+          </HomeWrapper>
+        </HomeSection>
+      </HeroSection>
+    </>
+  );
 };
 
 export default Home;
-
-
-
 
 // return (
 //   <main>
