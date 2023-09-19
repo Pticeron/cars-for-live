b9fc4afaa882179591b969b2674281445580a68e
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-09-19 13:38:36 +0300

    fixed styled

--- src/components/services/Payment.jsx
+++ src/components/services/Payment.jsx
@@ -1,21 +1,20 @@
 import {ContentWrapper, ServiceText, SecondaryText} from '../../components/services/Services.styled.js'
 
 const Payment = () => {
-    return (
-      <ContentWrapper>
-        <ServiceText>Умови оплати</ServiceText>
-        <SecondaryText>
-        Платіжні картки Visa/Mastercard
+  return (
+    <ContentWrapper>
+      <ServiceText>Умови оплати</ServiceText>
+      <SecondaryText>
+      Платіжні картки Visa/Mastercard
 Безготівковий розрахунок із ПДВ
 Apple Pay та Google Pay
 Оплата онлайн на сайті
-        </SecondaryText>
-        <SecondaryText>
-        Водії, що видають автомобілі, завжди мають бездротові платіжні термінали.
-        </SecondaryText>
-      </ContentWrapper>
-    );
-  };
-  
-  export default Payment;
-  
+      </SecondaryText>
+      <SecondaryText>
+      Водії, що видають автомобілі, завжди мають бездротові платіжні термінали.
+      </SecondaryText>
+    </ContentWrapper>
+  );
+};
+
+export default Payment;
