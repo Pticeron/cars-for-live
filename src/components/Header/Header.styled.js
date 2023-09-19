3f0b69070d9378d40a6371f2cdce99603da6be69
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-09-19 18:41:55 +0300

    add navigation

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const AppBar = styled.header`
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  backdrop-filter: blur(10px);
+  background-color: #00466e1a;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 8px 5px,
+    rgba(0, 0, 0, 0.1) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
+`;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,54 @@
+import { useLocation } from 'react-router-dom';
+import {
+  GoBackIcon,
+  GoBackLink,
+  HomeIcon,
+  Link,
+  LogoLink,
+  LogoText,
+  LogoWrapper,
+  Nav,
+  NavBlock,
+  NavList,
+  NavListItem,
+} from './Navigation.styled';
+import { logo } from '../../images/auto.png';
+
+const Navigation = () => {
+  const location = useLocation();
+  const isCatalogOrFavoritePage =
+    location.pathname === '/catalog' || location.pathname === '/favorites';
+
+  return (
+    <Nav>
+      <NavList>
+        <NavBlock>
+          <NavListItem>
+            <Link>
+              <HomeIcon />
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link to="/catalog">Catalogue</Link>
+          </NavListItem>
+          <NavListItem>
+            <Link to="/favorites">Favorites</Link>
+          </NavListItem>
+        </NavBlock>
+        <LogoLink to="/">
+          <LogoWrapper>
+            <img src={logo} alt="Auto Logo" width={60} height={40} />
+            <LogoText>InHheRoad</LogoText>
+          </LogoWrapper>
+        </LogoLink>
+        {isCatalogOrFavoritePage && (
+          <GoBackLink to="/">
+            <GoBackIcon />
+          </GoBackLink>
+        )}
+      </NavList>
+    </Nav>
+  );
+};
+
+export default Navigation;
