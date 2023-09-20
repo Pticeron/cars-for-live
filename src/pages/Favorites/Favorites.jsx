018e070baa70e4a76892bc91f4f85141ffc74e3b
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-09-20 08:24:12 +0300

    add page favorite

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,18 @@
-const Favorites =  () => {
+import React from 'react';
+import Container from "components/Container/Container";
+import { FilterSection } from "components/Filter/Filter";
+import { useSelector } from "react-redux";
 
+const Favorites =  ({ data }) => {
+const favorite = useSelector(state => state.favorite);
+const favoriteCars = data.filter(car => favorite.includes(car.id));
+return (
+    <>
+        <Container>
+            <FilterSection data={favoriteCars} />
+        </Container>
+    </>
+)
 }
 
 export default Favorites;
