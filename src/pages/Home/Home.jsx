9a59638a41a4563a993d0650f5247fa70e1504be
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-09-20 08:02:33 +0300

    change home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,35 +20,12 @@ const Home = ({ data }) => {
                 У сучасному суспільстві такі цінності як час і комфорт все
                 більше виходять на передній план, тому така послуга як оренда
                 автомобіля – вже перестала бути розкішшю, а стала щоденною
-                необхідністю. Залежно від ситуації покупка машини не завжди є
-                доцільним вкладенням коштів. Наприклад, для гостей Києва в
-                туристичних, або бізнес цілях найкращим рішенням буде подобовий
-                прокат авто – це недорого для вирішення тимчасових питань. Для
-                резидентів столиці дана послуга також буде корисна під
-                здійснення різного роду поїздок по Україні: оренда авто на
-                весілля, відпочинок, фотосесію або просто в розважальних цілях.
-                Життя непередбачуване і ніколи не знаєш, в яких ситуаціях може
-                терміново знадобиться прокат автомобіля. Для вирішення даної
-                проблеми існує сервіс компанії CarForLive – послуги автопрокату.
-                Ми пропонуємо взяти машину в оренду на найвигідніших умовах –
-                від 18 $ на добу.
+                необхідністю. 
               </HomeTextMain>
               <HomeTextSecondary>
                 In modern society, such values as time and comfort increasingly
                 come to the fore, so a service such as car rental has ceased to
-                be a luxury, but has become a daily necessity. Depending on the
-                situation, buying a car is not always an appropriate investment.
-                For example, for guests of Kyiv for tourist or business
-                purposes, the best solution would be daily car rental - it is
-                inexpensive to solve temporary issues. For residents of the
-                capital, this service will also be useful for making various
-                types of trips around Ukraine: renting a car for a wedding,
-                vacation, photo shoot or just for entertainment purposes. Life
-                is unpredictable and you never know in which situations you may
-                urgently need a car rental. To solve this problem, there is a
-                service of the CarForLive company - car rental services. We
-                offer to rent a car on the most favorable terms - from $18 per
-                day.
+                be a luxury, but has become a daily necessity. 
               </HomeTextSecondary>
             </ContentWrapper>
           </HomeWrapper>
